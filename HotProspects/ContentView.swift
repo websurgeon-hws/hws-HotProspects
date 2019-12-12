531dd67cd55ab2dc92d98898aeac576b27da9643
Peter Barclay <peter@peter-barclay.com>
2019-12-12 08:17:20 +0000

    Manually publishing ObservableObject changes

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -4,53 +4,27 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
 
-struct ContentView: View {
-    
-    var body: some View {
-        Text("Hello, World!")
-        .onAppear {
-            self.fetchData(from: "https://www.apple.com") { result in
-                switch result {
-                case .success(let str):
-                    print(str)
-                case .failure(let error):
-                    switch error {
-                    case .badURL:
-                        print("Bad URL")
-                    case .requestFailed:
-                        print("Bad URL")
-                    case .unknown:
-                        print("Unknown error")
-                    }
-                }
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
             }
         }
     }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+}
 
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
+struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
-
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+    var body: some View {
+        Text("Value is: \(updater.value)")
     }
 }
 
